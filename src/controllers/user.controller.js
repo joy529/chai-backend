e29a05f4fad923a8f4fa83af54c591cc740d26a2
joy jain <joyshah204@gmail.com>
2024-10-21 21:21:42 +0530

    check if user already exists: username, email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js"; 
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
 
 
@@ -32,6 +33,14 @@ const registerUser = asyncHandler( async (req, res) => {
         [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
     }
 
 })
