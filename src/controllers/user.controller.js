17f5faf545cc9dfa136498e3946e01b362e0919b
joy jain <joyshah204@gmail.com>
2024-10-23 15:00:26 +0530

    debug the logoutUser set to unset

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -159,9 +159,13 @@ const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1 // this removes the field from document
             }
+
+            // $set: {
+            //     refreshToken: undefined
+            // }
         },
         {
             new: true
